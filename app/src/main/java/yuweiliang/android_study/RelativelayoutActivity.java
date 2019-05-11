ddeeecb42f5e19c1eaa7751b67f84bb3765c45e2
liangyuwei <liangyuwei@yahoo.com>
2019-05-11 19:27:46 +0800

    Tried frame layout, relative layout and absolute layout.

---
+++ app/src/main/java/yuweiliang/android_study/RelativelayoutActivity.java
@@ -0,0 +1,13 @@
+package yuweiliang.android_study;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativelayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+    }
+}
